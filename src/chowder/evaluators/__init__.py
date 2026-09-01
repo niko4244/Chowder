3b1f7fa397aa676360c04c02c60382ce16618971
niko4244 <112509534+niko4244@users.noreply.github.com>
2026-09-01 09:54:43 -0500

    Export lm-eval evaluator

--- src/chowder/evaluators/__init__.py
+++ src/chowder/evaluators/__init__.py
@@ -1,5 +1,11 @@
 """Independent evaluation backends for Chowder."""
 
+from .lm_eval import LmEvalEvaluator, LmEvalSpec
 from .transformers_text import TransformersTextEvaluator, TransformersTextEvalSpec
 
-__all__ = ["TransformersTextEvaluator", "TransformersTextEvalSpec"]
+__all__ = [
+    "LmEvalEvaluator",
+    "LmEvalSpec",
+    "TransformersTextEvaluator",
+    "TransformersTextEvalSpec",
+]
